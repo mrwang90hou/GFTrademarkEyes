1bcc98cf883d697cd5109451b33fa5b10fc6fb4e
mrwang90hou <mrwang90hou@163.com>
2018-03-27 00:15:04 +0800

    【检测升级】初设

---
+++ GFTrademarkQueryAPP/Controller/LeftView/菜单/UpdateController.m
@@ -0,0 +1,30 @@
+//
+//  UpdateController.m
+//  GFTrademarkQueryAPP
+//
+//  Created by 王宁 on 2018/3/26.
+//  Copyright © 2018年 王宁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "UpdateController.h"
+@interface UpdateController()
+
+@end
+
+@implementation UpdateController
+
+-(void)viewDidLoad{
+    [super viewDidLoad];
+    self.title = @"检测升级";
+    //设置背景颜色:黄色
+    self.view.backgroundColor = [UIColor whiteColor];
+    //初始化控件
+    [self initView];
+    
+}
+-(void)initView{
+    
+}
+@end
+
